ba06c9bd4e6a844dce4d2187c9ba5588313ae6f7
cgprasanth <cgprasanth0330@gmail.com>
2017-06-28 14:47:31 +0530

    Add files via upload

---
+++ Player24.java
@@ -0,0 +1,10 @@
+package cgprasanthGuvi;
+import java.util.Scanner;
+public class Player24 {
+	public static void main(String[] args){
+		Scanner ss=new Scanner(System.in);
+		String s=ss.nextLine();
+		System.out.println(s.matches("-?\\d+(\\.\\d+)?"));
+    }
+
+}
