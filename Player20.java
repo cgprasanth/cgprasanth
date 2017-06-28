01931438c5d3e5c408d8a1ad72662a4cf7404cac
cgprasanth <cgprasanth0330@gmail.com>
2017-06-28 13:34:33 +0530

    Add files via upload

---
+++ Player20.java
@@ -0,0 +1,12 @@
+package cgprasanthGuvi;
+public class Player20 {
+	public static void main(String[] args){
+		String str1="1234";
+		String str2="45673";
+		int n1=Integer.parseInt(str1);
+		int n2=Integer.parseInt(str2);
+		int mul=n1*n2;
+		System.out.println(String.valueOf(mul));
+	}
+
+}
