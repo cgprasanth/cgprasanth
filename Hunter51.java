82efd919f0bbf025226320e67f1caab482ad595c
cgprasanth <cgprasanth0330@gmail.com>
2017-07-03 17:22:14 +0530

    Add files via upload

---
+++ Hunter51.java
@@ -0,0 +1,11 @@
+package cgprasanthGuvi;
+import java.util.Scanner;
+public class Hunter51 {
+	public static void main(String[] args){
+		Scanner ss=new Scanner(System.in);
+		String a=ss.nextLine();
+		String b=ss.nextLine();
+		System.out.println(String.valueOf(Integer.parseInt(a)*Integer.parseInt(b)));
+	}
+
+}
